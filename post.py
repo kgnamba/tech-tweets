d18f9395902aa70a810405e3154f5026b691540a
Kelsey <kn2426@columbia.edu>
2020-02-19 03:02:39 -0500

    fixed issue with auth

--- post.py
+++ post.py
@@ -4,6 +4,8 @@ import os
 from dotenv import load_dotenv
 import tweepy 
 
+load_dotenv()
+
 consumer_key =  os.getenv("consumer_key")
 consumer_secret = os.getenv("consumer_secret")
 access_token = os.getenv("access_token")
