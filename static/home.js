4e16d61f7b0c85014fe940e47f981527e0769b4d
Kelsey <kn2426@columbia.edu>
2020-03-10 15:18:14 -0400

    create_account added

--- static/home.js
+++ static/home.js
@@ -15,10 +15,29 @@ var run_auth = function(){
     });
   }
 
+var create_account = function(){
+  $.ajax({
+    type: "POST", url: "create_account",
+    success: function (result) {
+        console.log(result);
+        var w = window.location = result
+      },
+      error: function (request, status, error) {
+        console.log("Error");
+        console.log(request)
+        console.log(status)
+        console.log(error)
+      }
+  })
+}
+
 $(document).ready(function(){
 
     $("#log-in-btn").click(function(){
       run_auth()
     });
 
+    $('#create-account-btn').click(function(){
+      create_account()
+    })
 });
