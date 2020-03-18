a6dc87ba70f25d000b8b0945eca567a3ccc1c4d6
kgero <katystreet@gmail.com>
2020-03-18 14:20:41 -0400

    export to file on server

--- worksheet.py
+++ worksheet.py
@@ -16,6 +16,16 @@ CORS(app)
 def hello_world():
     return render_template('worksheet.html')
 
+@app.route('/savefile', methods=['POST'])
+def savefile():
+    text = request.values.get('text')
+    name = request.values.get('name')
+    with open(f'data/{name}.txt', 'w') as fle:
+        fle.write(text)
+    data = {'response': 'success'}
+    resp = Response(json.dumps(data), status=200, mimetype='application/json')
+    return resp
+
 if __name__ == "__main__":
 
     app.run(debug=True, port=8888)
