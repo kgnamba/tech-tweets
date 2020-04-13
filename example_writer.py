6918b0760a6850af72f5955f3662433c84a04539
kgero <katystreet@gmail.com>
2020-04-13 13:57:29 -0400

    baseline interface to write w just examples

---
+++ example_writer.py
@@ -0,0 +1,31 @@
+import json
+import os
+import pickle
+import requests
+import sys
+
+from flask import Flask, Response, render_template, request
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('example_writer.html')
+
+@app.route('/savefile', methods=['POST'])
+def savefile():
+    text = request.values.get('text')
+    name = request.values.get('name')
+    with open(f'data/{name}.txt', 'w') as fle:
+        fle.write(text)
+    data = {'response': 'success'}
+    resp = Response(json.dumps(data), status=200, mimetype='application/json')
+    return resp
+
+if __name__ == "__main__":
+
+    app.run(debug=True, port=8888)
